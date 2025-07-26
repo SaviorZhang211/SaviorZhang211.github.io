ca9b2cc410fb683f72faf591c7175c167b7bff80
SaviorZhang211 <2905002077@qq.com>
2025-07-26 22:56:14 +0800

    Site updated: 2025-07-26 22:56:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/19/article1/","2025/07/17/notice/"];function toRandomPost(){
+var posts=["2025/07/19/article1/","2025/07/17/notice/","2025/07/26/article2/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
